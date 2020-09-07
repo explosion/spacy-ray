1d87fc82d5ff55b39bfd8c7530a8cd82c4c42de9
Matthew Honnibal <honnibal+gh@gmail.com>
2020-09-08 01:18:03 +0200

    Update train_cli

--- spacy_ray/train_cli.py
+++ spacy_ray/train_cli.py
@@ -1,14 +1,21 @@
-from spacy.cli._app import app, Arg, Opt
+import typer
+import logging
 import ray
-import time
-import os
+from pathlib import Path
+from spacy import util
+from typing import Optional
+from spacy.cli.train import import_code, parse_config_overrides
+from spacy.cli._app import app, Arg, Opt
 
-from .worker import Worker, Evaluater 
+from .worker import Worker, Evaluater
+from .thinc_remote_params import SharedOptimizer
+
 
 
 @app.command("ray-train")
 def ray_train_cli(
     # fmt: off
+    ctx: typer.Context, # This is only used to read additional arguments
     config_path: Path = Arg(..., help="Path to config file", exists=True),
     code_path: Optional[Path] = Opt(None, "--code-path", "-c", help="Path to Python file with additional code (registered functions) to be imported"),
     verbose: bool = Opt(False, "--verbose", "-V", "-VV", help="Display more information for debugging purposes"),
@@ -18,12 +25,9 @@ def ray_train_cli(
     # fmt: on
 ):
     util.logger.setLevel(logging.DEBUG if verbose else logging.ERROR)
-    verify_cli_args(config_path, output_path)
     import_code(code_path)
-    config = load_config(
-        config_path,
-        overrides=parse_config_overrides(ctx.args),
-        interpolate=True
+    config = util.load_config(
+        config_path, overrides=parse_config_overrides(ctx.args), interpolate=True
     )
     distributed_setup_and_train(config, ray_address, use_gpu, num_workers)
 
@@ -34,22 +38,16 @@ def distributed_setup_and_train(config, use_gpu, num_workers, ray_address):
     else:
         ray.init(ignore_reinit_error=True)
 
-    RemoteWorker = ray.remote(Worker).options(
-        num_gpus=int(use_gpu >= 0),
-        num_cpus=2
-    )
+    RemoteWorker = ray.remote(Worker).options(num_gpus=int(use_gpu >= 0), num_cpus=2)
     workers = [
-        RemoteWorker.remote(
-            rank,
-            num_workers,
-            use_gpu,
-            config_path,
-            config_overrides
-        )
+        RemoteWorker.remote(rank, num_workers, use_gpu, config)
         for rank in range(num_workers)
     ]
     evaluater = ray.remote(Evaluater).remote()
-    conn = ray.remote(SharedParams).options(num_gpus=0).remote()
+    conn = ray.remote(SharedOptimizer).options(num_gpus=0).remote(
+        ray.get(workers[0].get_optimizer.remote()),
+        ray.get(workers[0].get_quorum.remote())
+    )
     futures = []
     for i, w in enumerate(workers):
         futures.append(w.train.remote(use_gpu, conn, evaluater))

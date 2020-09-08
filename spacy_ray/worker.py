4deb712651ad6d23754cc2059ebf5a28ecf47501
Matthew Honnibal <honnibal+gh@gmail.com>
2020-09-08 14:09:53 +0200

    Add type annotations

--- spacy_ray/worker.py
+++ spacy_ray/worker.py
@@ -1,43 +1,67 @@
 import time
 import os
+from pathlib import Path
+from typing import Optional, List, Tuple, Dict, Union, Callable, Any
+from thinc.config import Config
 from spacy.cli._util import get_sourced_components
 from spacy.cli.train import msg, train_while_improving, load_from_paths
 from spacy.cli.train import create_train_batches, create_evaluation_callback
 from spacy.cli.train import update_meta
 from spacy import util
-from thinc.api import require_gpu, use_pytorch_for_gpu_memory
+from spacy.language import Language
+from spacy.gold import Corpus
+from thinc.api import require_gpu, use_pytorch_for_gpu_memory, Optimizer
 from .thinc_remote_params import RayProxy, set_params_proxy
 
 
 class Worker:
-    def __init__(self, rank, num_workers, use_gpu, config, ray=None):
+    """Actor class for parallel training."""
+    rank: int
+    num_workers: int
+    gpu_id: int
+    nlp: Language
+    config: Config
+    _results: List
+    _evaluation_callback: Any
+
+    def __init__(
+        self,
+        config: Config,
+        rank: int=0,
+        num_workers: int=1,
+        use_gpu: int=0,
+        ray=None
+    ):
         if ray is None:
-            import ray
+            # Avoid importing ray in the module. This allows a test-ray to
+            # be passed in, and speeds up the CLI.
+            import ray # type: ignore
             self.ray = ray
         self.rank = rank
         self.num_workers = num_workers
         self.gpu_id = self._resolve_gpu(use_gpu)
         self.nlp, self.config = self._load_nlp_and_config(config)
         self._initialize_models(self.nlp, self.config)
-        self._evaluation_callback = None
+        self._evaluation_callback = lambda: {}
         self._results = []
+        self._has_evaluation_callback = False
 
-    def get_optimizer(self):
+    def get_optimizer(self) -> Optimizer:
         return self.config["training"]["optimizer"]
 
-    def get_corpus(self):
+    def get_train_corpus(self) -> Corpus:
         return self.config["training"]["train_corpus"]
 
     def get_dev_corpus(self):
         return self.config["training"]["dev_corpus"]
 
-    def get_quorum(self):
+    def get_quorum(self) -> int:
         # Default to setting the 'quorum' to be the number of workers multiplied
         # by the accumulate_gradient value. This is how many gradients for a
         # parameter we will accumulate before running the optimizer.
         return self.num_workers * self.config["training"]["accumulate_gradient"]
 
-    def train(self, use_gpu, conn, evaluater):
+    def train(self, use_gpu: bool, conn, evaluater) -> None:
         def evaluate():
             if self.rank == 0:
                 scores = self.evaluate()
@@ -77,25 +101,26 @@ class Worker:
                 info["words"] *= self.num_workers
                 print_row(info)
 
-    def evaluate(self):
-        if self._evaluation_callback is None:
+    def evaluate(self) -> Dict[str, Union[Dict[str, float], float]]:
+        if not self._has_evaluation_callback:
             self._evaluation_callback = create_evaluation_callback(
                 self.nlp,
                 self.get_dev_corpus(),
                 self.config["training"]["score_weights"],
             )
+            self._has_evaluation_callback = True
         return self._evaluation_callback()
 
-    def save_checkpoint(self, info, output_path):
+    def save_checkpoint(self, info: Dict, output_path: Path) -> None:
         update_meta(self.config["training"], self.nlp, info)
         self.nlp.to_disk(output_path)
 
-    def get_training_config(self):
+    def get_training_config(self) -> Config:
         return self.config["training"]
 
-    def _resolve_gpu(self, use_gpu):
+    def _resolve_gpu(self, use_gpu: int) -> int:
         if use_gpu >= 0:
-            gpu_id = os.environ.get("CUDA_VISIBLE_DEVICES")
+            gpu_id = int(os.environ.get("CUDA_VISIBLE_DEVICES", -1))
             msg.info(f"Using GPU (isolated): {gpu_id}")
             require_gpu(0)
         else:
@@ -103,7 +128,7 @@ class Worker:
             gpu_id = -1
         return gpu_id
 
-    def _load_nlp_and_config(self, config):
+    def _load_nlp_and_config(self, config: Config) -> Tuple[Language, Config]:
         if config.get("training", {}).get("seed") is not None:
             util.fix_random_seed(config["training"]["seed"])
         if config.get("system", {}).get("use_pytorch_for_gpu_memory"):
@@ -114,7 +139,7 @@ class Worker:
             util.load_vectors_into_model(nlp, config["training"]["vectors"])
         return nlp, config
 
-    def _initialize_models(self, nlp, config):
+    def _initialize_models(self, nlp: Language, config: Config) -> None:
         optimizer = config["training"]["optimizer"]
         # Components that shouldn't be updated during training
         frozen_components = config["training"]["frozen_components"]
@@ -127,7 +152,7 @@ class Worker:
             with nlp.select_pipes(enable=resume_components):
                 nlp.resume_training(sgd=optimizer)
 
-        corpus = self.get_corpus()
+        corpus = self.get_train_corpus()
         train_examples = list(corpus(nlp))
         with nlp.select_pipes(disable=[*frozen_components, *resume_components]):
             nlp.begin_training(lambda: train_examples)
@@ -142,7 +167,7 @@ class Worker:
         if weights_data is not None:
             raise NotImplementedError
 
-    def _set_params_proxies(self, nlp, conn):
+    def _set_params_proxies(self, nlp: Language, conn) -> None:
         proxy = RayProxy(conn, ray=self.ray)
         for name, component in nlp.pipeline:
             if hasattr(component, "model"):

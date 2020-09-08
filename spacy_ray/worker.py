8ef3cbb80c511e5c878dba8e6354e271b44befc9
Matthew Honnibal <honnibal+gh@gmail.com>
2020-09-08 03:09:21 +0200

    Fixes to spacy ray

--- spacy_ray/worker.py
+++ spacy_ray/worker.py
@@ -72,6 +72,7 @@ class Worker:
             print_row, finalize_logger = self.config["training"]["logger"](self.nlp)
         for batch, info, is_best_checkpoint in training_step_iterator:
             if self.rank == 0 and is_best_checkpoint is not None:
+                print(info)
                 info["words"] *= self.num_workers
                 print_row(info)
 

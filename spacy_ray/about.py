42b299b4656a1545bad43e400835423f54ebaf19
Matthew Honnibal <honnibal+gh@gmail.com>
2020-09-08 12:59:15 +0200

    Add about

---
+++ spacy_ray/about.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.0"
+__author__ = "ExplosionAI GmbH"
+__email__ = "contact@explosion.ai"
+__uri__ = "https://github.com/honnibal/spacy-ray"
+__license__ = "MIT"
+__summary__ = "spaCy plugin for parallel processing with Ray"

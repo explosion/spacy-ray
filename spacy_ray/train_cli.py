1507ff1afae1d37afab5cba1087d59c85f3d4061
Ines Montani <ines@ines.io>
2020-09-13 19:36:46 +0200

    Tidy up

--- spacy_ray/train_cli.py
+++ spacy_ray/train_cli.py
@@ -6,13 +6,13 @@ from pathlib import Path
 from spacy import util
 from spacy.cli._util import import_code, parse_config_overrides, Arg, Opt, app
 from thinc.api import require_gpu, Config
+from wasabi import msg
 
 from .worker import Worker, Evaluater
 
 
-RAY_HELP = """Command-line interface for parallel and distributed computing via
-Ray. Assumes Ray is installed and that the cluster is initialized. See the
-Ray documentation for details: https://ray.io.
+RAY_HELP = """CLI for parallel and distributed computing via
+Ray. See the Ray documentation for details: https://ray.io.
 """
 
 # Create our subcommand, and install it within spaCy's CLI
@@ -27,18 +27,21 @@ def ray_train_cli(
     # fmt: off
     ctx: typer.Context,  # This is only used to read additional arguments
     config_path: Path = Arg(..., help="Path to config file", exists=True),
-    code_path: Optional[Path] = Opt(None, "--code-path", "-c", help="Path to Python file with additional code (registered functions) to be imported"),
+    code_path: Optional[Path] = Opt(None, "--code", "-c", help="Path to Python file with additional code (registered functions) to be imported"),
     verbose: bool = Opt(False, "--verbose", "-V", "-VV", help="Display more information for debugging purposes"),
-    strategy: str = Opt("peer_params", "--strategy", "-s", help="Which strategy to use"),
     use_gpu: int = Opt(-1, "--gpu-id", "-g", help="GPU ID or -1 for CPU"),
     num_workers: int = Opt(1, "--n-workers", "-w", help="Number of workers"),
-    ray_address: str = Opt("", "--address", "-a", help="Address of ray cluster"),
+    ray_address: Optional[str] = Opt(None, "--address", "-a", help="Address of ray cluster"),
     # fmt: on
 ):
     """
-    Train a spaCy model using Ray for parallel training.
+    Train a spaCy pipeline using Ray for parallel training.
     """
-    require_gpu(0)
+    if use_gpu >= 0:
+        msg.info("Using GPU")
+        require_gpu(use_gpu)
+    else:
+        msg.info("Using CPU")
     util.logger.setLevel(logging.DEBUG if verbose else logging.ERROR)
     import_code(code_path)
     config_overrides = parse_config_overrides(ctx.args)
@@ -47,13 +50,13 @@ def ray_train_cli(
 
 
 def ray_train(
-    config: Config, *, ray_address: str = "", num_workers: int = 1, use_gpu: int = -1
+    config: Config, *, ray_address: Optional[str] = None, num_workers: int = 1, use_gpu: int = -1
 ) -> None:
     # We're importing Ray here so it doesn't need to be imported when spaCy /
     # spaCy's CLI is imported (which would otherwise take too long)
     import ray
 
-    if ray_address:
+    if ray_address is not None:
         ray.init(address=ray_address)
     else:
         ray.init(ignore_reinit_error=True)

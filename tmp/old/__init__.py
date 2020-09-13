3140041c2824e38ba594087246947ae11bc68a0d
Ines Montani <ines@ines.io>
2020-09-13 23:13:20 +0200

    Remove tmp

--- tmp/old/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import train_cli  # noqa
--- tmp/old/about.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.0.0"
-__author__ = "ExplosionAI GmbH"
-__email__ = "contact@explosion.ai"
-__uri__ = "https://github.com/honnibal/spacy-ray"
-__license__ = "MIT"
-__summary__ = "spaCy plugin for parallel processing with Ray"
